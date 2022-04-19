74c60864508ebfcf9fafe20f02fbdf053537a533
unanmed <1319491857@qq.com>
2022-04-19 19:53:25 +0800

    fix

--- comment/wanning/index.js
+++ comment/wanning/index.js
@@ -1,19 +1,21 @@
 const isWifi = navigator.connection.type === 'wifi';
 const bgms = [
-	'Collapsing World.mp3', 
-	'Grasswalk.mp3', 
-	'Horizon.mp3', 
-	'Illusionary Daytime.mp3', 
-	'Lumina.mp3', 
-	'My Sunset.mp3', 
-	'Rags To Rings.mp3', 
-	'Starset.mp3', 
-	'theme of SSS.mp3', 
-	'Wings of Piano.mp3', 
-	'夏恋.mp3', 
-	'苍穹.mp3'
+    'Collapsing World.mp3',
+    'Grasswalk.mp3',
+    'Horizon.mp3',
+    'Illusionary Daytime.mp3',
+    'Lumina.mp3',
+    'My Sunset.mp3',
+    'Rags To Rings.mp3',
+    'Starset.mp3',
+    'theme of SSS.mp3',
+    'Wings of Piano.mp3',
+    '夏恋.mp3',
+    '苍穹.mp3'
 ];
 const tower = 'wanning';
+
+console.log(isWifi);
 
 if (isWifi) {
     const length = bgms.length;
